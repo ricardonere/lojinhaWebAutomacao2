3403d5652b3fcf3c691642873389c14940fbd18c
Ricardo Nere <ricardonere@gmail.com>
2022-04-22 18:56:52 -0300

    Primeiro commit lojinhaWeb2

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,50 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
+
+import java.time.Duration;
+
+@DisplayName("Testes Web no Modulo de Produtos")
+public class ProdutosTest {
+    private WebDriver navegador;
+    @BeforeEach
+    public void beforeEach(){
+        //Abrir um navegador
+        this.navegador = new ChromeDriver();
+        //Vou maximizar a tela
+        this.navegador.manage().window().maximize();
+        //Vou definir um tempo de espera padrão de 5 segundos
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //Vou navegar para a a pagina da lojinha web
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2/");
+    }
+@Test
+@DisplayName("Nao é permitido registrar produto com valor igual a zero")
+    public void testNaoEPermitidoRegistrarProdutoComValorIgualAZero(){
+    //Vou fazer login("Usando PAGE OBJECT E FLUENT PAGE para CONVERTER MUITAS LINHAS DE CÓDIGO EM POUCAS LINHAS")
+    String mensagemApresentada = new LoginPage(navegador)
+            .informarOUsuario("admin")
+                    .informarASenha("admin")
+                            .submeterFormularioDeLogin()
+                                    .acessarFormularioAdicaoNovoProduto()
+                                            .informarNomeDoProduto("Macbook Pro")
+                                                    .informarValorDoProduto("000")
+                                                            .informarCoresDoProduto("preto,branco")
+            .submeterFormularioDeAdicaoComErro()
+                    .capturarMensagemApresentada();
+
+    //vou validar que a mensagem de erro foi apresentada
+
+    Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+}
+@AfterEach
+    public void afterEach(){
+    //Vou fechar o navegador
+    navegador.quit();
+}
+}
