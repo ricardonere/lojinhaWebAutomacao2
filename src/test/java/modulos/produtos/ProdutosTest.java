e9247a7481daa49e0ef6c85c356037667f57d322
Ricardo Nere <ricardonere@gmail.com>
2022-04-24 20:54:26 -0300

    Test Produtos Lojinha Web2

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -42,6 +42,44 @@ public class ProdutosTest {
     Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
 
 }
+@Test
+@DisplayName("Nao e permitido registrar produto com valor acima de R$7.000,00 ")
+public void testNaoEPermitidoRegistrarProdutoComValorAcimaDeSeteMilReais(){
+        //Vou fazer login("Usando PAGE OBJECT E FLUENT PAGE para CONVERTER MUITAS LINHAS DE CÓDIGO EM POUCAS LINHAS")
+    String mensagemApresentada = new LoginPage(navegador)
+            .informarOUsuario("admin")
+            .informarASenha("admin")
+            .submeterFormularioDeLogin()
+            .acessarFormularioAdicaoNovoProduto()
+            .informarNomeDoProduto("Macbook Pro")
+            .informarValorDoProduto("700001")
+            .informarCoresDoProduto("preto,branco")
+            .submeterFormularioDeAdicaoComErro()
+            .capturarMensagemApresentada();
+
+    //vou validar que a mensagem de erro foi apresentada
+
+    Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+}
+@Test
+@DisplayName("Posso adicionar produtos que estejam na faixa de 0,01 a 7.000,00 reais ")
+    public void testPossoAdicionarProdutosComValorDeUmCentavoASeteMilReais(){
+String mensagemApresentada = new LoginPage(navegador)
+        .informarOUsuario("admin")
+        .informarASenha("admin")
+        .submeterFormularioDeLogin()
+        .acessarFormularioAdicaoNovoProduto()
+        .informarNomeDoProduto("IPHONE13")
+        .informarValorDoProduto("69990")
+        .informarCoresDoProduto("azul, vermelho")
+        .submeterFormularioDeAdicaoComSucesso()
+                .capturarMensagemApresentada();
+
+Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+//Produto adicionado com sucesso
+}
+
 @AfterEach
     public void afterEach(){
     //Vou fechar o navegador
